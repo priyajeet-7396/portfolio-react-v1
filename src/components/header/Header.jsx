c244b22a1792c2a068166490fbc618d1651499d8
unknown <priyajeetsaxena@gmail.com>
2023-04-10 11:45:58 +0530

    installing react icons and using them in socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -12,6 +13,7 @@ const Header = () => {
       <h1>Priyajeet Saxena</h1>
       <h5 className="text-light">Fullstack Developer</h5>
       <CTA />
+      <HeaderSocials />
       <div className='me'>
         <img src={ME} alt='me'/>
         <a href='#contact' className='scroll__down'>Scroll Down</a>
